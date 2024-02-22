219942c5d5a52d9f6233f880961a9c6171498488
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-02-22 17:31:52 +0530

    Login page set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import style from "./style/App.module.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-     hiiiii
-    </div>
+    <>
+      <div className={style.appContainer}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import style from "../style/Login.module.css";
+
+function Login() {
+  return (
+    <div className={style.outerDiv}>
+      <div className={style.loginDiv}>
+        <img src="/images/instalogo.jpeg" alt="instagram logo" />
+        <div className={style.loginDetails}>
+          <input
+            type="text"
+            name=""
+            placeholder="Phone number, username or email addess"
+          />
+          <input type="text" name="" placeholder="Password" />
+          <button>Log IN</button>
+        </div>
+        <div className={style.segregationDiv}>
+          <div className={style.emptyDiv}></div>
+          <p>OR</p>
+          <div className={style.emptyDiv}></div>
+        </div>
+        <div className={style.optionDiv}>
+          <img src="/images/fblogo.avif" alt="fb logo" />
+          <p>Log in with Facebook</p>
+        </div>
+        <p>Forgotten your password?</p>
+      </div>
+      <div className={style.signInDiv}>
+        <p>Don't have an account?Sign Up</p>
+      </div>
+      <div className={style.appDiv}>
+        <p>Get the App</p>
+        <div className={style.appDownloadDiv}>
+          <img src="/images/appstore.png" alt="get from app store" />
+          <img src="/images/playstore.png" alt="get from google play" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
