99b510f300cb05b4f2652a1c2aa52b990060820b
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-02-27 08:35:43 +0530

    On clicking "Search" the div styling updates,and consequent styling for others links added,with bottomsheet position updated acc to above styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import style from "../style/Navbar.module.css";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -18,8 +18,13 @@ import {
   faBars,
 } from "@fortawesome/free-solid-svg-icons";
 
-function Navbar({ toggleBottomSheet }) {
+function Navbar({ toggleBottomSheet, usedIn }) {
   const bottomSheetRef = useRef(null);
+  const [showPTags, setShowPTags] = useState(true);
+
+  const [instaIconPaddingLeft, setInstaIconPaddingLeft] = useState("20px");
+
+  const [sideBarDivPaddingRight, setSideBarDivPaddingRight] = useState("60px");
 
   useEffect(() => {
     function handleClickOutside(event) {
@@ -38,68 +43,97 @@ function Navbar({ toggleBottomSheet }) {
     };
   }, [toggleBottomSheet]);
 
+  const handleSearchClick = () => {
+    setShowPTags(!showPTags);
+    setInstaIconPaddingLeft(showPTags ? "0px" : "20px");
+    setSideBarDivPaddingRight(showPTags ? "0px" : "20px");
+  };
+
+  const handleNavLinkClick = () => {
+    setShowPTags(true);
+  };
+
   return (
-    <div className={style.sideBarDiv} ref={bottomSheetRef}>
+    <div
+      className={style.sideBarDiv}
+      ref={bottomSheetRef}
+      style={{
+        paddingRight: sideBarDivPaddingRight,
+      }}
+    >
       <NavLink to="/home">
-        <FontAwesomeIcon icon={faInstagram} className={style.sideBarIcon} />
+        <FontAwesomeIcon
+          onClick={handleNavLinkClick}
+          icon={faInstagram}
+          className={style.sideBarIcon}
+          style={{
+            paddingLeft: instaIconPaddingLeft,
+          }}
+        />
       </NavLink>
       <div className={style.menuList}>
         <div className={style.menuItem}>
-          <FontAwesomeIcon icon={faHouse} />
+          <FontAwesomeIcon onClick={handleNavLinkClick} icon={faHouse} />
           <NavLink to="/home" className={style.navlink}>
-            <p>Home </p>
+            {showPTags && <p>Home</p>}
+          </NavLink>
+        </div>
+
+        <div className={style.menuItem} onClick={handleSearchClick}>
+          <FontAwesomeIcon
+            onClick={handleNavLinkClick}
+            icon={faMagnifyingGlass}
+          />
+          <NavLink to="" className={style.navlink}>
+            {showPTags && <p>Search</p>}
           </NavLink>
         </div>
 
         <div className={style.menuItem}>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-          <NavLink to="/search" className={style.navlink}>
-            <p>Search </p>
-          </NavLink>
-        </div>
-        <div className={style.menuItem}>
-          <FontAwesomeIcon icon={faCompass} />
+          <FontAwesomeIcon onClick={handleNavLinkClick} icon={faCompass} />
           <NavLink to="/explore" className={style.navlink}>
-            <p>Explore </p>
+            {showPTags && <p>Explore</p>}
           </NavLink>
         </div>
         <div className={style.menuItem}>
-          <FontAwesomeIcon icon={faVideo} />
+          <FontAwesomeIcon onClick={handleNavLinkClick} icon={faVideo} />
           <NavLink to="/reels" className={style.navlink}>
-            <p>Reels </p>
+            {showPTags && <p>Reels</p>}
           </NavLink>
         </div>
         <div className={style.menuItem}>
-          <FontAwesomeIcon icon={faFacebookMessenger} />
+          <FontAwesomeIcon
+            icon={faFacebookMessenger}
+          />
           <NavLink to="/message" className={style.navlink}>
-            <p>Message </p>
+            {showPTags && <p>Message</p>}
           </NavLink>
         </div>
         <div className={style.menuItem}>
           <FontAwesomeIcon icon={faHeart} />
           <NavLink to="/notification" className={style.navlink}>
-            <p>Notification </p>
+            {showPTags && <p>Notification</p>}
           </NavLink>
         </div>
 
         <div className={style.menuItem}>
           <FontAwesomeIcon icon={faSquarePlus} />
           <NavLink to="/Create" className={style.navlink}>
-            <p>Create </p>
+            {showPTags && <p>Create</p>}
           </NavLink>
         </div>
 
         <div className={style.menuItem}>
-          <FontAwesomeIcon icon={faUser} />
-          <NavLink to="/profile" className={style.navlink}>
-            <p>Profile </p>
+          <FontAwesomeIcon onClick={handleNavLinkClick} icon={faUser} />
+          <NavLink to="/Create" className={style.navlink}>
+            {showPTags && <p>Profile</p>}
           </NavLink>
         </div>
 
         <div className={style.menuItem} onClick={toggleBottomSheet}>
           <FontAwesomeIcon icon={faBars} />
           <NavLink to="" className={style.navlink}>
-            <p>More</p>
+            {showPTags && <p>More</p>}
           </NavLink>
         </div>
       </div>
